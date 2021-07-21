bbef988684eecb410c044ec4e9adddb10907903c
kwangho kwak <678hoa@gmail.com>
2021-07-21 19:32:52 +0900

    add bootstrp, controller class, simple html files

---
+++ src/main/java/org/zerock/guestbook1/Guestbook1Application.java
@@ -0,0 +1,13 @@
+package org.zerock.guestbook1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guestbook1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Guestbook1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/guestbook1/Guestbook1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.guestbook1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guestbook1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
